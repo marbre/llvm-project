5aba00de74048ff120491b8d09a35066076173c5
Marius Brehler <marius.brehler@iml.fraunhofer.de>
2023-02-01 20:10:47 +0000

    [mlir][standalone] Extend sample with custom type

---
+++ mlir/examples/standalone/include/Standalone/StandaloneTypes.h
@@ -0,0 +1,12 @@
+
+
+
+#ifndef STANDALONE_STANDALONETYPES_H
+#define STANDALONE_STANDALONETYPES_H
+
+#include "mlir/IR/BuiltinTypes.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "Standalone/StandaloneOpsTypes.h.inc"
+
+#endif // STANDALONE_STANDALONETYPES_H
